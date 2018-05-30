fa8b6a8443f5bf3bf9ce12e39de41d14354e369e
siphera <salmansiphenkosi@gmail.com>
2018-05-30 07:05:11 +0200

    ....

---
+++ js/registration_number.js
@@ -0,0 +1,10 @@
+var plateInput = document.querySelector(".plateInputField"),
+    addBtn = document.querySelector(".regAddBtn"),
+    list = document.querySelector("#list");
+
+function reg_Numbers(data) {
+    var storedPlates = {},
+        displayPlates = "",
+        
+    
+}

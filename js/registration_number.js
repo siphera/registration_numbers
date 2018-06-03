ec0bdc4853ec6baacdcfc55a97c6e7caf90eb5ee
siphera <salmansiphenkosi@gmail.com>
2018-06-03 23:06:38 +0200

    modifued dom file

--- js/registration_number.js
+++ js/registration_number.js
@@ -1,7 +1,3 @@
-var plateInput = document.querySelector(".plateInputField"),
-    addBtn = document.querySelector(".regAddBtn"),
-    list = document.querySelector("#list");
-
 function reg_Numbers(data) {
     var storedPlates = {};
         
---
+++ js/registration_numbers_DOM.js
@@ -0,0 +1,3 @@
+var plateInput = document.querySelector(".plateInputField"),
+    addBtn = document.querySelector(".regAddBtn"),
+    list = document.querySelector("#list");

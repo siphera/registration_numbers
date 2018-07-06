0be0c79f116a137e2613ab024cc40fedcced8e43
siphera <salmansiphenkosi@gmail.com>
2018-07-06 17:06:19 +0200

    added logic

---
+++ js/registration_number.js
@@ -0,0 +1,48 @@
+function reg_numFF(stored) {
+  var regist = "";
+  var plateList = {};
+
+  if (stored) {
+    for (let i = 0; i < stored.length; i++){
+      let current = stored[i];
+      plateList[current] = 0;
+    }
+  }
+
+  function setReg(num) {
+    if(plateList[num] === undefined && (num.startsWith('CA') ||
+        num.startsWith('CL') || num.startsWith('CJ') )){
+      regist = num;
+      plateList[regist] = 0;
+      return true;
+    }
+    return false;
+  }
+
+  function getReg() {
+    return regist;
+  }
+
+  function getRegMap() {
+    return Object.keys(plateList);
+  }
+
+  function townFilter(town) {
+    let regs = Object.keys(plateList);
+
+    if(town === 'ALL')
+      return regs;
+
+    let result = regs.filter(function(regist){
+      return regist.startsWith(town);
+    });
+    return result;
+  }
+
+  return {
+    setReg,
+    getReg,
+    getRegMap,
+    townFilter
+  }
+}

ea4508768a589b7d920658bff9ba55324b88be93
siphera <salmansiphenkosi@gmail.com>
2018-07-11 17:48:25 +0200

    tests

---
+++ test/registration_number.test.js
@@ -0,0 +1,17 @@
+describe('Registration Numbers Application Tests', function() {
+    it('should return registration number that starts with "CA" only', function() {
+        
+    });
+    
+    it('should return registration number that starts with "CL" only', function() {
+        
+    });
+    
+    it('should return registration number that starts with "CJ" only', function() {
+        
+    });
+    
+    it('should return registration number that starts with "CAW" only', function() {
+        
+    });
+})

de37123fbd1e69db6a80ed79d0c410dc6070863f
siphera <salmansiphenkosi@gmail.com>
2018-07-11 16:57:51 +0200

    ...

--- js/registration_number.js
+++ js/registration_number.js
@@ -1,48 +1,48 @@
 function reg_numFF(stored) {
-  var regist = "";
-  var plateList = {};
+    var regist = "";
+    var plateList = {};
 
-  if (stored) {
-    for (let i = 0; i < stored.length; i++){
-      let current = stored[i];
-      plateList[current] = 0;
+    if (stored) {
+        for (let i = 0; i < stored.length; i++) {
+            let current = stored[i];
+            plateList[current] = 0;
+        }
     }
-  }
 
-  function setReg(num) {
-    if(plateList[num] === undefined && (num.startsWith('CA') ||
-        num.startsWith('CL') || num.startsWith('CJ') )){
-      regist = num;
-      plateList[regist] = 0;
-      return true;
+    function setReg(num) {
+        if (plateList[num] === undefined && (num.startsWith("CA ") ||
+                num.startsWith("CL") || num.startsWith("CJ") || num.startsWith("CAW"))) {
+            regist = num;
+            plateList[regist] = 0;
+            return true;
+        }
+        return false;
     }
-    return false;
-  }
 
-  function getReg() {
-    return regist;
-  }
+    function getReg() {
+        return regist;
+    }
 
-  function getRegMap() {
-    return Object.keys(plateList);
-  }
+    function getRegMap() {
+        return Object.keys(plateList);
+    }
 
-  function townFilter(town) {
-    let regs = Object.keys(plateList);
+    function townFilter(town) {
+        let regs = Object.keys(plateList);
 
-    if(town === 'ALL')
-      return regs;
+        if (town === 'ALL')
+            return regs;
 
-    let result = regs.filter(function(regist){
-      return regist.startsWith(town);
-    });
-    return result;
-  }
+        let result = regs.filter(function (regist) {
+            return regist.startsWith(town);
+        });
+        return result;
+    }
 
-  return {
-    setReg,
-    getReg,
-    getRegMap,
-    townFilter
-  }
+    return {
+        setReg,
+        getReg,
+        getRegMap,
+        townFilter
+    }
 }

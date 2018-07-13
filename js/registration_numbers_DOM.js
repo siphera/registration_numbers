dde99e695fe083a3ac9505e29c1d81cc44b04196
siphera <salmansiphenkosi@gmail.com>
2018-07-13 14:21:20 +0200

    finished testing

--- js/registration_numbers_DOM.js
+++ js/registration_numbers_DOM.js
@@ -8,7 +8,6 @@ let plateInput = document.querySelector(".plateInputField"),
 let itemsArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
 var registration = reg_numFF(itemsArray);
 
-
 //----------restrict input to only take uppercase--------
 var input = document.getElementsByTagName("input")[0]
 input.oninput = function () {
@@ -20,15 +19,14 @@ function liMaker(text) {
     let li = document.createElement('li');
     li.innerText = text;
     display.appendChild(li);
-    //    auto erase input field
+//-----------auto erase input field-----------
     plateInput.value = "";
 }
-
 
 function addReg() {
     var input = plateInput.value.toUpperCase();
     if (registration.setReg(input)) {
-// push new value to the array
+//---------push new value to the array-----------
         itemsArray.push(plateInput.value);
         localStorage.setItem('items', JSON.stringify(itemsArray));
         liMaker(plateInput.value);
@@ -39,14 +37,13 @@ function addReg() {
     }
 };
 
-// loop to display stored list to user interface
+//--------loop to display stored list to user interface--------
 window.addEventListener('load', function () {
     if (itemsArray.length > 0)
         for (var i = 0; i < itemsArray.length; i++) {
             liMaker(itemsArray[i]);
         }
 });
-
 
 addBtn.addEventListener('click', addReg);
 
@@ -57,7 +54,6 @@ clearBtn.addEventListener('click', function () {
         display.removeChild(display.firstChild);
     }
 });
-
 
 filterTown.addEventListener('change', function () {
     let filt = registration.townFilter(filterTown.value);
--- test/registration_number.test.js
+++ test/registration_number.test.js
@@ -1,17 +1,82 @@
-describe('Registration Numbers Application Tests', function() {
-    it('should return registration number that starts with "CA" only', function() {
-        
+describe('Registration Numbers Application Tests', function () {
+    it('should return registration number that starts with "CA" only', function () {
+        let registration = reg_numFF();
+
+        registration.setReg("CA 123-456");
+        assert.equal(registration.getReg(), "CA 123-456");
     });
-    
-    it('should return registration number that starts with "CL" only', function() {
-        
+
+    it('should return registration number that starts with "CL" only', function () {
+        let registration = reg_numFF();
+
+        registration.setReg("CL 456-123");
+        assert.equal(registration.getReg(), "CL 456-123");
+
     });
-    
-    it('should return registration number that starts with "CJ" only', function() {
-        
+
+    it('should return registration number that starts with "CJ" only', function () {
+        let registration = reg_numFF();
+
+        registration.setReg("CJ 456-123");
+        assert.equal(registration.getReg(), "CJ 456-123");
+
     });
-    
-    it('should return registration number that starts with "CAW" only', function() {
-        
+
+    it('should return registration number that starts with "CAW" only', function () {
+        let registration = reg_numFF();
+
+        registration.setReg("CAW 456-123");
+        assert.equal(registration.getReg(), "CAW 456-123");
+
     });
 })
+
+//------------------------------------------------
+
+describe('Testing the filter', function () {
+    it('should return an array of registration plates that starts with "CA" only', function () {
+        let registration = reg_numFF();
+
+        registration.setReg("CA 123-456");
+        registration.setReg("CA 321-456");
+        registration.setReg("CA 123-321");
+        assert.deepEqual(registration.townFilter("CA"), ["CA 123-456","CA 321-456","CA 123-321"]);
+    });
+    
+    it('should return an array of registration plates that starts with "CL" only', function () {
+        let registration = reg_numFF();
+
+        registration.setReg("CL 123-456");
+        registration.setReg("CL 321-456");
+        registration.setReg("CL 123-321");
+        assert.deepEqual(registration.townFilter("CL"), ["CL 123-456","CL 321-456","CL 123-321"]);
+    });
+    
+    it('should return an array of registration plates that starts with "CJ" only', function () {
+        let registration = reg_numFF();
+
+        registration.setReg("CJ 123-456");
+        registration.setReg("CJ 321-456");
+        registration.setReg("CJ 123-321");
+        assert.deepEqual(registration.townFilter("CJ"), ["CJ 123-456","CJ 321-456","CJ 123-321"]);
+    });
+    
+    it('should return an array of registration plates that starts with "CAW" only', function () {
+        let registration = reg_numFF();
+
+        registration.setReg("CAW 123-456");
+        registration.setReg("CAW 321-456");
+        registration.setReg("CAW 123-321");
+        assert.deepEqual(registration.townFilter("CAW"), ["CAW 123-456","CAW 321-456","CAW 123-321"]);
+    });
+    
+    it('should return an array of all registration plates', function () {
+        let registration = reg_numFF();
+
+        registration.setReg("CA 123-456");
+        registration.setReg("CJ 321-456");
+        registration.setReg("CAW 123-321");
+        registration.setReg("CL 123-321");
+        assert.deepEqual(registration.townFilter("ALL"), ["CA 123-456","CJ 321-456","CAW 123-321","CL 123-321"]);
+    });
+});
